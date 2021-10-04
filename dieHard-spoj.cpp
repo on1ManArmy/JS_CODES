5c31479d527fbe3f5907b0de53197d80bdbded31
abhisheksawan <56653520+abhisheksawan@users.noreply.github.com>
2021-10-04 23:42:08 +0530

    Added DieHard spoj solution

---
+++ dieHard-spoj.cpp
@@ -0,0 +1,33 @@
+// radhe radhe
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define inf 1e18
+#define abs(a,b) a > b ? a - b : b - a
+
+
+int main(){
+	int t;
+	cin>>t;
+
+	while(t--){
+
+		int H,A;
+		cin>>H>>A;
+		int start = 0;
+		int cnt = 0;
+		while(1){
+			if(start == 0){
+				H = H -20;
+				A = A + 5;
+				cnt++;
+			}
+
+			if(start == 1){
+				
+			}
+		}
+	}
+	return 0;
+}
